c889d3815625a2dba8672f5848a5a9dc0ee5529c
abdulla1395 <abdullahfx95@gmail.com>
2025-07-05 20:54:10 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-export default function Home() { return <h1>مرحبا بك في قولها</h1>; }
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>مرحبا بك في قولها</h1>
+      <p>منصة توليد شخصيات عربية تتكلم باللهجة</p>
+    </div>
+  );
+}
